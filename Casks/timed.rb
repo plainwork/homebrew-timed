4155f39d7884943189fe819174b4ba30ee63e1f5
Mark Garrigan <mrkgrrgn@gmail.com>
2026-01-09 05:52:18 -0600

    first commit

---
+++ Casks/timed.rb
@@ -0,0 +1,11 @@
+cask "timed" do
+  version "0.0.0"
+  sha256 "REPLACE_ME"
+
+  url "https://github.com/plainwork/timed/releases/download/v#{version}/Timed-#{version}.zip"
+  name "Timed"
+  desc "Menu bar timer"
+  homepage "https://github.com/plainwork/timed"
+
+  app "Timed.app"
+end
